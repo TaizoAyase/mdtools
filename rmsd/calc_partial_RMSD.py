32b82438c1a13350364add0d6fd43afdf5369914
Taizo_Ayase <zxcvbnmkind@gmail.com>
2016-01-29 02:03:32 +0900

    refactoring

--- rmsd/calc_partial_RMSD.py
+++ rmsd/calc_partial_RMSD.py
@@ -11,8 +11,8 @@ reference_file = '/path/to/reference/pdb_or_gro'
 structure_file = reference_file
 trajectory_file = '/path/to/trajectory/file'
 
-sel_fit = 'protein and name CA'
-sel_calc = {
+fit = 'protein and name CA'
+calc = {
     'TM1': '101:133',
     'TM2': '137:157',  # etc...
 }
@@ -23,16 +23,6 @@ rmsd_out = 'rmsd.csv'
 plotting = True
 
 #############
-
-
-def fit(P, Q):
-    # P:mobile, Q:reference
-    P -= centroid(P)
-    Q -= centroid(Q)
-    rot = kabsch(P, Q)
-    moved = np.dot(P, rot)
-    return(moved)
-
 
 def centroid(X):
     C = np.sum(X, axis=0) / len(X)
@@ -64,7 +54,7 @@ def rmsd(calc, ref):
 ##################
 
 ### main ###
-# 1. Load
+# 1. Load Universe
 sys.stderr.write('Loading files ...\n')
 ref_uni = Universe(reference_file)
 uni = Universe(structure_file, trajectory_file)
@@ -74,18 +64,18 @@ uni = Universe(structure_file, trajectory_file)
 # 2.1. reference
 sys.stderr.write('Preparing selections ...\n')
 
-ref = ref_uni.select_atoms(sel_fit)  # for overall fitting
+sel_ref = ref_uni.select_atoms(fit)  # for overall fitting
 
 ref_selections = {}  # for each calc elements reference
-for k, v in sel_calc.items():
+for k, v in calc.items():
     sel = ref_uni.select_atoms(selection_tmpl % v)
     ref_selections[k] = sel
 
 # 2.2. mobile
-mob = uni.select_atoms(sel_fit)  # for overall fitting
+sel_mob = uni.select_atoms(fit)  # for overall fitting
 
 selections = {}  # for each calc elements
-for k, v in sel_calc.items():
+for k, v in calc.items():
     sel = uni.select_atoms(selection_tmpl % v)
     selections[k] = sel
 
@@ -94,12 +84,12 @@ for k, v in sel_calc.items():
 t_tot = uni.trajectory.n_frames
 z = np.zeros(t_tot)
 results = {}
-for k in sel_calc.keys():
+for k in calc.keys():
     results[k] = z.copy()
 
 
 # 4. reference coordinate sets
-ref_coord = ref.coordinates()
+ref_coord = sel_ref.coordinates()
 com_ref = centroid(ref_coord)
 ref_coord -= com_ref
 ref_coord_sets = {}
@@ -112,14 +102,14 @@ for k, v in ref_selections.items():
 for ts in uni.trajectory:
     sys.stderr.write("Calc for %d/%d ... \r" % (ts.frame, t_tot))
 
-    mob_coord = mob.coordinates()
+    mob_coord = sel_mob.coordinates()
     com_mob = centroid(mob_coord)
     mob_coord -= com_mob
 
     rot_matrix = kabsch(mob_coord, ref_coord)
 
-    for k, sel_calc in selections.items():
-        cal_coord = sel_calc.coordinates()
+    for k, sel in selections.items():
+        cal_coord = sel.coordinates()
         cal_coord -= com_mob
         moved_coord = np.dot(cal_coord, rot_matrix)
         val = rmsd(moved_coord, ref_coord_sets[k])
@@ -152,7 +142,7 @@ t = np.arange(t_tot)
 sbn.plt.hold(False)
 for k, v in results.items():
     sbn.tsplot(v, time=t)
-    sbn.plt.title('RMSD of %s; fitting %s' % (k, sel_fit))
+    sbn.plt.title('RMSD of %s; fitting %s' % (k, fit))
     sbn.plt.xlabel('Time step')
     sbn.plt.ylabel('RMSD (A)')
     sbn.plt.savefig('RMSD_%s.png' % k)
