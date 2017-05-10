0c9c48e72a0abb51b1c3620d62b21f7a8f1aaf67
TaizoAyase <zxcvbnmkind@gmail.com>
2017-05-10 15:49:33 +0900

    update PCA script

--- pca/pca.py
+++ pca/pca.py
@@ -3,7 +3,7 @@
 from __future__ import print_function
 from mdtools.rms_tools import fit, centroid, kabsch, rmsd, superpose
 from tqdm import tqdm
-from MDAnalysis import *
+import mdtraj as md
 import numpy as np
 import scipy.linalg
 import sys
@@ -25,6 +25,10 @@ reference_file = '/path/to/reference/pdb_or_gro'
 structure_file = reference_file
 trajectory_file = '/path/to/trajectory/file'
 
+reference_file = '../test_files/chainB.gro'
+structure_file = reference_file
+trajectory_file = '../test_files/chainB.xtc'
+
 fit = 'name CA'
 
 cov_out = 'covar.csv'
@@ -37,31 +41,32 @@ eigval_threshold = 90  # % of total was displayed
 prcomp = 5  # max num of components to write
 scale_factor = 5  # for showing vectors in qsc
 
+chunk_size = 1
 
 ##################
 
 # loading trajectory
 sys.stderr.write('Loading Universe ...\n')
-uni = Universe(structure_file, trajectory_file)
-uni_ref = Universe(reference_file)
+ref = md.load(reference_file)
 
-sel_ca = uni.select_atoms(fit)
-sel_ca_ref = uni_ref.select_atoms(fit)
+sel_ca = ref.topology.select(fit)
+sel_ca_ref = ref.topology.select(fit)
 
-if sel_ca.n_atoms != sel_ca_ref.n_atoms:
+if len(sel_ca) != len(sel_ca_ref):
     raise RuntimeError('Atom number is not matched.')
 
-ref_coord = sel_ca_ref.positions
-n_frames = uni.trajectory.n_frames
-n_atoms = sel_ca_ref.n_atoms
+ref_coord = ref.xyz[0, sel_ca_ref, :] * 10. # nm to A
+n_frames = 0
+n_atoms = len(sel_ca)
 dof = n_atoms * 3
 
 # make average coordinate
 coord_ave = np.zeros(dof)
 sys.stderr.write('Calc average coordinate...\n')
-for ts in tqdm(uni.trajectory):
-    coord = superpose(sel_ca.positions, ref_coord).flatten()
+for chunk in tqdm(md.iterload(trajectory_file, top=structure_file, chunk=chunk_size)):
+    coord = superpose(chunk.xyz[0, sel_ca_ref, :] * 10, ref_coord).flatten()
     coord_ave += coord
+    n_frames += chunk.n_frames
 coord_ave /= n_frames
 
 
@@ -69,11 +74,12 @@ coord_ave /= n_frames
 # sum(deviation x deviation) / frames
 sys.stderr.write('Calc covariance matrix ...\n')
 deviation_all = np.zeros((n_frames, dof))
-uni.trajectory[0]
-for ts in tqdm(uni.trajectory):
-    coord = superpose(sel_ca.positions, ref_coord).flatten()
+i = 0
+for chunk in tqdm(md.iterload(trajectory_file, top=structure_file, chunk=chunk_size), total=n_frames):
+    coord = superpose(chunk.xyz[0, sel_ca_ref, :] * 10, ref_coord).flatten()
     deviation = coord - coord_ave
-    deviation_all[ts.frame] = deviation
+    deviation_all[i] = deviation
+    i += 1
 
 
 sys.stderr.write('Building covariance matrix ...\n')
@@ -110,7 +116,7 @@ f = open(vec_out, 'w+')
 
 for i, vec in enumerate(vectors[::-1]):
     f.write('@%d comp\n' % i)
-    for j in xrange(n_atoms):
+    for j in range(n_atoms):
         vec_x = vec[3*j + 0]
         vec_y = vec[3*j + 1]
         vec_z = vec[3*j + 2]
@@ -128,8 +134,8 @@ np.savetxt(proj_out, proj_vec[:, :prcomp:-1], delimiter=', ')
 
 # write out qsc-file to visualize
 sys.stderr.write('Write out the QSC file ...\n')
-prot_sel = uni_ref.select_atoms('protein')
-prot_sel.write(pdb_out, format='PDB')
+ref.save_pdb(pdb_out)
+
 
 f = open(qsc_out, 'w+')
 
@@ -154,9 +160,10 @@ f.write(header)
 rend_line = '\t\t<renderer type="atomintr" color="#BF0000" mode="fancy" showlabel="false" stipple0="1000.0" name="dom%d" visible="false" width="0.3">\n'
 vect_line = '\t\t<line pos1="(%3.5f, %3.5f, %3.5f)" pos2="(%3.5f, %3.5f, %3.5f)"/>\n'
 tail_line = '\t\t</renderer>\n'
-ref_coord = sel_ca_ref.positions
+#ref_coord = sel_ca_ref.positions
+ref_coord = ref.xyz[0, sel_ca_ref, :] * 10 # nm to A
 
-for i in xrange(prcomp):
+for i in range(prcomp):
     f.write(rend_line % i)
     vec = vectors[:, -(i+1)].reshape(n_atoms, 3)
     for j, v in enumerate(vec):
