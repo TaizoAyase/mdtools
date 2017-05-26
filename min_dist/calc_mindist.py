aa721bd88352476c20fec43975a9afd9e9fa5d98
TaizoAyase <zxcvbnmkind@gmail.com>
2017-05-26 14:52:15 +0900

    move residue table to mdtools module

--- min_dist/calc_mindist.py
+++ min_dist/calc_mindist.py
@@ -3,6 +3,7 @@
 from __future__ import print_function
 import numpy as np
 import sys
+import mdtools.constants as const
 from MDAnalysis import *
 from itertools import product
 import matplotlib
@@ -34,55 +35,10 @@ trajectory_file = '../test_files/chainB.xtc'
 #############
 if include_mainchain:
     # atom list (O/N only)
-    resid_dict = {
-        'ALA': ['N', 'O'],
-        'CYS': ['N', 'O', 'SG'],
-        'ASP': ['N', 'O', 'OD1', 'OD2'],
-        'ASPP': ['N', 'O', 'OD1', 'OD2'],
-        'GLU': ['N', 'O', 'OE1', 'OE2'],
-        'GLUP': ['N', 'O', 'OE1', 'OE2'],
-        'PHE': ['N', 'O'],
-        'GLY': ['N', 'O'],
-        'HIS': ['N', 'O', 'ND1', 'NE2'],  # normal His
-        'HSD': ['N', 'O', 'ND1', 'NE2'],  # neutral His proton on ND1
-        'HDE': ['N', 'O', 'ND1', 'NE2'],  # neutral His proton on NE2
-        'HSP': ['N', 'O', 'ND1', 'NE2'],  # protonated His
-        'ILE': ['N', 'O'],
-        'LYS': ['N', 'O', 'NZ'],
-        'LEU': ['N', 'O'],
-        'MET': ['N', 'O', 'SD'],
-        'ASN': ['N', 'O', 'OD1', 'ND2'],
-        'PRO': ['N', 'O'],
-        'GLN': ['N', 'O', 'OE1', 'NE2'],
-        'ARG': ['N', 'O', 'NE', 'NH1', 'NH2'],
-        'SER': ['N', 'O', 'OG'],
-        'THR': ['N', 'O', 'OG1'],
-        'VAL': ['N', 'O'],
-        'TRP': ['N', 'O', 'NE1'],
-        'TYR': ['N', 'O', 'OH'],
-    }
+    resid_dict = const.RESID_DICT
 else:
     # atom list (sidechain only)
-    resid_dict = {
-        'CYS': ['SG'],
-        'ASP': ['OD1', 'OD2'],
-        'ASPP': ['OD1', 'OD2'],
-        'GLU': ['OE1', 'OE2'],
-        'GLUP': ['OE1', 'OE2'],
-        'HIS': ['ND1', 'NE2'],  # normal His
-        'HSD': ['ND1', 'NE2'],  # neutral His proton on ND1
-        'HDE': ['ND1', 'NE2'],  # neutral His proton on NE2
-        'HSP': ['ND1', 'NE2'],  # protonated His
-        'LYS': ['NZ'],
-        'MET': ['SD'],
-        'ASN': ['OD1', 'ND2'],
-        'GLN': ['OE1', 'NE2'],
-        'ARG': ['NE', 'NH1', 'NH2'],
-        'SER': ['OG'],
-        'THR': ['OG1'],
-        'TRP': ['NE1'],
-        'TYR': ['OH'],
-    }
+    resid_dict = const.RESID_DICT_SIDECHAIN
 
 # load data
 sys.stderr.write('Loading data ...\n')
