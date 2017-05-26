f1791ca315936f0ecdfb5e4cfa1a8fd660a4180b
TaizoAyase <zxcvbnmkind@gmail.com>
2017-05-26 17:36:14 +0900

    update mindist script

--- min_dist/calc_mindist.py
+++ min_dist/calc_mindist.py
@@ -4,8 +4,10 @@ from __future__ import print_function
 import numpy as np
 import sys
 import mdtools.constants as const
-from MDAnalysis import *
-from itertools import product
+import mdtraj as md
+import itertools
+from tqdm import tqdm
+
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
@@ -27,65 +29,69 @@ target_pairs = [
 include_mainchain = True
 plotting = True
 
-#structure_file = '/path/to/structure/file'
-#trajectory_file = '/path/to/trajectory/file'
-structure_file = '../test_files/chainB.gro'
-trajectory_file = '../test_files/chainB.xtc'
+structure_file = '/path/to/structure/file'
+trajectory_file = '/path/to/trajectory/file'
+
+chunk_size = 100
 
 #############
+
 if include_mainchain:
-    # atom list (O/N only)
+    # atom list (including O and N of backbone)
     resid_dict = const.RESID_DICT
 else:
     # atom list (sidechain only)
     resid_dict = const.RESID_DICT_SIDECHAIN
 
-# load data
+# load the structure file data
 sys.stderr.write('Loading data ...\n')
-uni = Universe(structure_file, trajectory_file)
+ref = md.load(structure_file)
 
 # prepare selection
 sys.stderr.write('Preparing the selection array...\n')
 calc_selections = []
 for lst in target_pairs:
-    sel = uni.select_atoms(
-        *['protein and resid %d and name CA' % i for i in lst])
+    sel = ref.topology.select("name CA and (residue %d or residue %d)" % tuple(lst))
+    ca_atom1 = ref.topology.atom(sel[0])
+    ca_atom2 = ref.topology.atom(sel[1])
 
     # raise error if selection was invalid
-    if sel.n_atoms != 2:
+    if len(sel) != 2:
         raise RuntimeError
 
+    # get atom names list from the dictionary
     try:
-        atomset1 = resid_dict[sel.resnames[0]]
-        atomset2 = resid_dict[sel.resnames[1]]
+        atomset1 = resid_dict[ca_atom1.residue.name]
+        atomset2 = resid_dict[ca_atom2.residue.name]
     except KeyError as e:
-        sys.stderr.write(
-            '!!! Hydrogen bonding atom is not defined in selected residues. !!!')
-        sys.stderr.write(
-            'If you want to calc distance between main-chain, set "include_mainchain = True"')
+        sys.stderr.write('!!! Hydrogen bonding atom is not defined in selected residues. !!!')
+        sys.stderr.write('If you want to calc distance between main-chain, set "include_mainchain = True"')
         raise e
 
-    atomset_iter = product(atomset1, atomset2)
+    atomset_iter = itertools.product(atomset1, atomset2)
 
     selections_tmp = []
     for atoms in atomset_iter:
-        s = uni.select_atoms(
-            *['protein and resid %d and name %s' % (i, a) for (i, a) in zip(lst, atoms)])
+        inputs = (ca_atom1.residue.resSeq, atoms[0], ca_atom2.residue.resSeq, atoms[1])
+        s = ref.topology.select('(residue %d and name %s) or (residue %d and name %s)' % inputs)
         selections_tmp.append(s)
 
     calc_selections.append(selections_tmp)
 
 # calc
-t_tot = uni.trajectory.n_frames
-t_ary = np.zeros((t_tot, len(target_pairs)))
-
-for ts in uni.trajectory:
-    sys.stderr.write('Calc for frame %d/%d ...\r' % (ts.frame, t_tot))
-    # iterate over selection_pairs
+sys.stderr.write('Start distance calculation...\n')
+results = [[] for _ in calc_selections]
+itertraj = md.iterload(trajectory_file, top=ref, chunk=chunk_size)
+for chunk in tqdm(itertraj):
     for i, sel in enumerate(calc_selections):
-        d_ary = [s.bond.length() for s in sel]
-        min_d = min(d_ary)
-        t_ary[ts.frame, i] = min_d
+        coor = chunk.xyz[:, sel, :] # 4-dimensional ary: (time, #of selection pair, 2atoms, xyz)
+        dist_vectors = coor[:, :, 0, :] - coor[:, :, 1, :] # 3-dimensional ary: (time, #of pair, xyz)
+        dists = np.linalg.norm(dist_vectors, axis=2) # calc norm over xyz
+        min_dist = np.min(dists, axis=1) # select the minimum value in the pairs -> 1-dimensional ary
+        results[i].append(min_dist)
+
+# convert into 2-dim numpy array
+t_ary = np.array([np.concatenate(e) for e in results]).T
 
 sys.stderr.write('\n')
 
@@ -98,14 +104,15 @@ if not plotting:
     sys.exit(0)
 
 # plotting
-t = np.arange(t_tot)
+n_frames = t_ary.shape[0]
+t = np.arange(n_frames)
 for i in range(len(target_pairs)):
     plt.clf()
     title = 'mindist_%d-%d' % tuple(target_pairs[i])
     plt.plot(t, t_ary[:, i])
     plt.title(title)
     plt.xlabel('Time step')
-    plt.ylabel('Minimum dist (A)')
+    plt.ylabel('Minimum dist (nm)')
     plt.savefig('%s.png' % title)
     sys.stderr.write('%s.png saved !\n' % title)
 
